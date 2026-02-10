47105d50ad3fa1b6ff95566620f4e1260ce7422b
Mixy <merzlikinmihail29@gmail.com>
2026-02-10 21:21:18 +0300

    Code changed

--- agent/graph.py
+++ agent/graph.py
@@ -12,12 +12,12 @@ class Topic(BaseModel):
 class GraphNode(BaseModel):
     node_id: str
     topic_id: str
-    studied: bool
+    is_studied: bool
     is_major: bool
-    prev_node: GraphNode | None
-    next_node: GraphNode | None
+    prev_node: str | None
+    next_node: str | None
 
 
 class Graph(BaseModel):
     graph_id: str
-    first_node: GraphNode
+    first_node: str
