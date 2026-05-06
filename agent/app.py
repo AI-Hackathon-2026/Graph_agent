7db077c3a7af97104d8b198b68119ae2572c28b5
mixy <merzlikinmihail29@gmail.com>
2026-05-06 19:48:32 +0300

    added optimization

--- agent/app.py
+++ agent/app.py
@@ -1,9 +1,10 @@
 from typing import Any, Callable, Type, cast
 
 import aiohttp
+from aiohttp import TCPConnector
 from pydantic import ValidationError
 
-from agent.config import application_hosts_setting
+from agent.config import application_hosts_setting, kafka_settings
 from agent.dto import (
     CreateCourseRequest,
     CreateCourseResponse,
@@ -15,19 +16,25 @@ from agent.dto import (
     GetTopicResponse,
     ResponseCodes,
 )
-from agent.main import metrics_collector
+from agent.kafka_producer import KafkaProducer
 
 
 class OrchestratorClient:
     session: aiohttp.ClientSession
 
+    def __init__(self, producer: KafkaProducer):
+        self.producer = producer
+
     async def start_http_session(self):
-        self.session = aiohttp.ClientSession()
+        connector = TCPConnector(
+            limit=2000,
+        )
+        self.session = aiohttp.ClientSession(connector=connector)
 
     async def close_http_session(self):
         await self.session.close()
 
-    @metrics_collector.metrics
+    # @metrics_collector.metrics
     async def request(
         self,
         request_class: Type[
@@ -45,7 +52,8 @@ class OrchestratorClient:
         url: str,
         body: dict,
         http_method: str,
-    ) -> CreateCourseResponse | GetGraphsResponse | GetTopicResponse | GetGraphsPreviewResponse:
+        key: str,
+    ):
         try:
             request_class(**body)
         except ValidationError:
@@ -63,6 +71,7 @@ class OrchestratorClient:
             "patch": self.session.patch,
         }
         try:
+            was_exception = False
             session_method = cast(
                 Callable[..., Any], method_mapping.get(http_method.lower())
             )
@@ -73,26 +82,36 @@ class OrchestratorClient:
             ) as response:
                 response.raise_for_status()
                 response = response_class(**await response.json())
-                return response
 
         except ValidationError:
             message = None
             status = ResponseCodes.INTERNAL_ERROR
+            was_exception = True
 
         except TimeoutError:
             message = None
             status = ResponseCodes.INTERNAL_ERROR
+            was_exception = True
 
         except aiohttp.ClientResponseError:
             message = None
             status = ResponseCodes.INTERNAL_ERROR
+            was_exception = True
 
         except aiohttp.ClientError:
             message = None
             status = ResponseCodes.INTERNAL_ERROR
-        return response_class(
-            **{"request_id": body["request_id"], "message": message, "status": status}
+            was_exception = True
+        if was_exception:
+            response = response_class(
+                **{
+                    "request_id": body["request_id"],
+                    "message": message,
+                    "status": status,
+                }
+            )
+        await self.producer.send_message(
+            topic=kafka_settings.PRODUCER_KAFKA_TOPIC,
+            key=key,
+            value=response.model_dump_json(),
         )
-
-
-orchestrator_client = OrchestratorClient()
